914a2fe33bb71c74ab4d5b08fb721d88b93b56bd
unknown <thug858@EN-283906.UoA.auckland.ac.nz>
2016-10-02 17:41:37 +1300

    added enemy with basic RNG, no AI

---
+++ Ups and Downs/Assets/Scripts/SphereEnemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereEnemy : MonoBehaviour {
+	
+	public float rate = 5.0f;
+	public Transform bubble;
+
+	// Use this for initialization
+	void Start() {
+		InvokeRepeating("SpawnSphere", 0.5f , 1.0f / rate);
+	}
+	
+	// Update is called once per frame
+	void SpawnSphere() {
+		Instantiate(bubble, Random.insideUnitSphere + transform.position, Quaternion.identity);
+		Debug.Log("this is going");
+	}
+}
