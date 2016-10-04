1f7df2016f00b1b54f1b542c3ec2decb38de290b
Michael Ieti <m.ieti001@gmail.com>
2016-10-04 17:19:47 +1300

    Add script for pushable blocks. Make a tag called MovingGround, for mobile platforms. Reshuffled some stuff in the hierarchy.

---
+++ Ups and Downs/Assets/Scripts/PushableObjectScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Attach this script to an object that you want to be pushed by a character.
+ * 
+ * */
+public class PushableObjectScript : Switch {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	public override void setActive(){
+		// forces on character also affect the block
+
+	}
+
+	public override void setDeactive(){
+		//forces on character now do not affect the block
+	}
+}
