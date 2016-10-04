bc8c6650ab33683536eb349c87f0e9ccfd1c786b
unknown <arran94@gmail.com>
2016-10-04 17:29:59 +1300

    Update coin to modify player score

--- Ups and Downs/Assets/Scripts/Coin.cs
+++ Ups and Downs/Assets/Scripts/Coin.cs
@@ -2,13 +2,29 @@
 using System.Collections;
 
 /**
-    The coin collectible provides a direct score benefit to the player that picks it up.
-
+    The coin collectible provides a direct score benefit when picked up.
 */
 public class Coin : Collectible {
+    public int score;
 
 	public override void onPickup() {
-		base.onPickup();
+        GameObject[] gameControllerList;
+        gameControllerList = GameObject.FindGameObjectsWithTag("GameController");
+        GameController controller = null;
+        foreach (GameObject obj in gameControllerList)
+        {
+            controller = obj.GetComponent<GameController>();
+            if (controller != null)
+            {
+                break;
+            }
+        }
+        if (controller == null)
+        {
+            Debug.LogError("Could not find active Game Controller Object");
+            return;
+        }
+        controller.addScore(score);
 		Debug.Log("Coin picked up");
 		//Play a coin-specific sound?
 	}
