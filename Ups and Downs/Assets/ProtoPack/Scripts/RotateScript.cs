863c814d67fe3e0e28e3fe023bc93ec53b88f9ad
MisterAndersen <benjamin.a.97@gmail.com>
2016-10-04 16:35:30 +1300

    Added up-and-down bobbing to RotateScript

--- Ups and Downs/Assets/ProtoPack/Scripts/RotateScript.cs
+++ Ups and Downs/Assets/ProtoPack/Scripts/RotateScript.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class RotateScript : MonoBehaviour {
 
-	void Update () {
+	float frameCount = 0;
 	
+	void Update () {
+		frameCount+= Time.deltaTime;
+		frameCount = frameCount % (2 * Mathf.PI);
 		transform.Rotate (Vector3.up*5);
-
+		transform.Translate(Vector3.up * Mathf.Sin(frameCount * 4) * 0.007f);
 	}
 }
