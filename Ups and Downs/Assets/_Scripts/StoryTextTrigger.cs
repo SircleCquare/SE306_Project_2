4d648b8a7c257d1ad38cae7287e1a6d2de1abbd4
Michael Ieti <m.ieti001@gmail.com>
2016-10-11 21:31:50 +1300

    Add the Trigger Text prefab. The prefab is composed of a trigger collider and a 3d Text Object. Add a custom 3D Text Shader.

---
+++ Ups and Downs/Assets/_Scripts/StoryTextTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoryTextTrigger : MonoBehaviour {
+
+	public GameObject[] storyText;
+
+	void Start () {
+		foreach (GameObject text in storyText) {
+			text.SetActive (false);
+		}
+	}
+	
+	void OnTriggerEnter(Collider other){
+		foreach (GameObject text in storyText) {
+			text.SetActive (true);
+		}
+	}
+}
