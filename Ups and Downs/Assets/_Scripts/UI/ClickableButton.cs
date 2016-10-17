1d9fdf6cc9195f9be397e83c400107e13ace1104
MisterAndersen <benjamin.a.97@gmail.com>
2016-10-18 11:39:43 +1300

    Added alternate blue health pack to freeze enemies on pickup. Needs to have hooks configured to the controller script to be created.

---
+++ Ups and Downs/Assets/_Scripts/UI/ClickableButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ClickableButton : MonoBehaviour {
+	
+	public int levelNumber = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void OnClick() {
+		SceneManager.LoadScene("Level " + levelNumber);
+	}
+}
