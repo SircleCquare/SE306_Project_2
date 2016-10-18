ada80c211469fa0e38eadb0a8734b790ea910d02
unknown <arran94@gmail.com>
2016-10-18 18:51:42 +1300

    Initial Prototype for force based movement

---
+++ Ups and Downs/Assets/_Scripts/Level Scripts/TestPlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class TestPlayerController : MonoBehaviour {
+    public float speed;
+    public float tilt;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        rb.velocity = movement * speed;
+    }
+}
