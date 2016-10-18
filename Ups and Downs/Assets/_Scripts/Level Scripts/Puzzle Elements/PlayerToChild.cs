486a57f9be30bbcd5e86a3feead5d919584eb45a
Michael Ieti <m.ieti001@gmail.com>
2016-10-18 17:50:45 +1300

    Add summary section and comments for PlayerToChild.cs

--- Ups and Downs/Assets/_Scripts/Level Scripts/Puzzle Elements/PlayerToChild.cs
+++ Ups and Downs/Assets/_Scripts/Level Scripts/Puzzle Elements/PlayerToChild.cs
@@ -1,6 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// PlayerToChild is a script to prevent characters and boxes slipping off moving platforms.<br>
+/// To use this script, create an empty child of a moving platform.
+/// Add a Trigger Collider to that child, where the collider mesh extends above the height of the platform.
+/// Then attach this script to that empty child.
+/// </summary>
 public class PlayerToChild : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other){
