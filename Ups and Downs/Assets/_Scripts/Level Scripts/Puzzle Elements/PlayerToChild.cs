96cf1f4db2ff5dbb2d0604a3df7836253e80da8a
Michael Ieti <m.ieti001@gmail.com>
2016-10-18 17:21:52 +1300

    Create horizontal platform test scene. Implement a way to make Characters stay on platforms. However, boxes won't stay on the platform.

---
+++ Ups and Downs/Assets/_Scripts/Level Scripts/Puzzle Elements/PlayerToChild.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerToChild : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag (GameController.PLAYER_TAG)) {
+			other.gameObject.transform.SetParent (this.gameObject.transform);
+		}
+		if (other.gameObject.CompareTag (GameController.WEIGHTED_TAG)) {
+			if (! other.gameObject.transform.parent.CompareTag (GameController.PLAYER_TAG)) {
+				other.gameObject.transform.SetParent (this.gameObject.transform);
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.gameObject.CompareTag (GameController.PLAYER_TAG)){
+			other.gameObject.transform.SetParent (null);
+		}
+		if (other.gameObject.CompareTag (GameController.WEIGHTED_TAG)) {
+			if (! other.gameObject.transform.parent.CompareTag (GameController.PLAYER_TAG)) {
+				other.gameObject.transform.SetParent (null);
+			}
+		}
+	}
+}
