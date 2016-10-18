dcbbfbdb8c32541ad153b2a608688d2c282807e5
unknown <arran94@gmail.com>
2016-10-18 20:23:02 +1300

    Added player jump

--- Ups and Downs/Assets/_Scripts/Level Scripts/TestPlayerController.cs
+++ Ups and Downs/Assets/_Scripts/Level Scripts/TestPlayerController.cs
@@ -5,19 +5,58 @@ using System.Collections.Generic;
 
 
 public class TestPlayerController : MonoBehaviour {
+    public float maxSpeed = 10f;
     public float speed;
-    public float tilt;
-    private Rigidbody rb;
 
+    public float jumpForce = 10f;
+    public float gravity = 5f;
+
+    private Rigidbody rb;
+    private GameController controller;
+    private float distToGround;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        controller = GameController.Singleton;
+        distToGround = GetComponent<Collider>().bounds.extents.y;
     }
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
-        rb.velocity = movement * speed;
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f) * speed;
+        movement.y = rb.velocity.y;
+        rb.velocity = movement;
+   
+
+        Vector3 clampedVelocity = rb.velocity;
+        clampedVelocity.x = Mathf.Clamp(clampedVelocity.x, -maxSpeed, maxSpeed);
+        clampedVelocity.y = Mathf.Clamp(clampedVelocity.y, -maxSpeed, maxSpeed);
+        clampedVelocity.z = 0;
+        rb.velocity = clampedVelocity;
+
+        if (IsGrounded())
+        {
+            if (controller.isJump())
+            {
+                jump();
+            }
+        } else
+        {
+            rb.AddForce(Vector3.down * gravity * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
     }
+
+    private bool IsGrounded() {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.2f - 1.5f);
+     }
+
+    private void jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+  
+
 }
