77c1c12623880dd9b25fd70d1c1c026bdf91dc61
Harsh Chokshi <harshchokshi@gmail.com>
2016-10-18 15:02:13 +1300

    Add missing semi-colon to invisibility collectible

--- Ups and Downs/Assets/_Scripts/Level Scripts/Collectibles/Invisibility.cs
+++ Ups and Downs/Assets/_Scripts/Level Scripts/Collectibles/Invisibility.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Invisibility : Collectible {
-    public float InvisiblityTime
+    public float InvisiblityTime;
 
     public override void onPickup() {
         base.onPickup();
