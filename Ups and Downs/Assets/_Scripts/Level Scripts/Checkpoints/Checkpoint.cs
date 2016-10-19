ebc0b1c6ce068bdaf0fdbb1bb475cb11583d6034
MisterAndersen <benjamin.a.97@gmail.com>
2016-10-20 11:23:15 +1300

    Added documentation for Level Scripts/Checkpoint and Level Scripts/Collectibles. Removed legacy method from Collectible and updated references to use GameController Singleton.

--- Ups and Downs/Assets/_Scripts/Level Scripts/Checkpoints/Checkpoint.cs
+++ Ups and Downs/Assets/_Scripts/Level Scripts/Checkpoints/Checkpoint.cs
@@ -1,11 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// A Checkpoint defines a location where a given player character should respawn at
+/// in their untimely death. This is used to enable easy level resetting.
+/// and could be used to provide save-points within a level (currently unused).
+/// </summary>
 public class Checkpoint : MonoBehaviour {
 
     /** Indictates which player can activate and use this checkpoint */
     public Side checkpointSide;
 
+    /// <summary>
+    /// The order represents which checkpoint in the sequence of checkpoints for
+    /// the level side this is. 0 represents the first checkpoint, 1 represents
+    /// the second checkpoint (unused) and so forth.
+    /// Matching nth checkpoints on both sides need to be on the same screen otherwise
+    /// the characters will be moed around upon respawning.
+    /// </summary>
     public int order = 0;
 
     /** purely visible for Debug purposes, DO NOT CHANGE */
@@ -14,7 +26,7 @@ public class Checkpoint : MonoBehaviour {
     void Awake()
     {
         GameController.Singleton.RegisterCheckpoint(this);
-        Debug.Log(GameController.Singleton.getCheckpoint(checkpointSide, 0));
+        //Debug.Log(GameController.Singleton.getCheckpoint(checkpointSide, 0));
     }
 
     public Vector3 getPosition()
--- Ups and Downs/Assets/_Scripts/Level Scripts/Collectibles/Collectible.cs
+++ Ups and Downs/Assets/_Scripts/Level Scripts/Collectibles/Collectible.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// The Collectible class provides a framework for all collectible items.
+/// When colliding with the player the onPickup() method is invoked.
+/// </summary>
 public class Collectible : MonoBehaviour {
-    
+
     private Vector3 originPosition;
     private Vector3 originRotation;
     private Vector3 originScale;
-    // To handle reset of collectibles on death
+
+    /// <summary>
+    /// Controls whether or not this collectible will be replaced on death (defaults to true)
+    /// </summary>
     public bool isResetOnDeath = true;
     private bool pickedUp = false;
 
+    /// <summary>
+    /// Set position, rotation and scale at runtime from the collectble's world position
+    /// (Used to replace the collectible).
+    /// </summary>
     protected virtual void Start()
     {
         originPosition = transform.position;
@@ -17,35 +28,42 @@ public class Collectible : MonoBehaviour {
         originScale = transform.localScale;
     }
 
-	
+  /// <summary>
+  /// When the active player collides with the collectible call onPickup().
+  /// </summary>
 	protected void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == GameController.PLAYER_TAG && !pickedUp)
-        {
-            onPickup();
-        }
-		
-    }
-	
-    public virtual void onPickup() {
-        if (!isResetOnDeath)
-        {
-            Destroy(this.gameObject);
-        }
-        GetComponent<Renderer>().enabled = false;
-        pickedUp = true;
-	}
-
-    protected GameController getGameController()
+    if (other.gameObject.tag == GameController.PLAYER_TAG && !pickedUp)
     {
-        return GameController.Singleton;
+      onPickup();
     }
 
-    public virtual void ResetBehaviour()
+  }
+
+  /// <summary>
+  /// Called when the collectible is picked up by the active player character.
+  /// </summary>
+  public virtual void onPickup() {
+
+    // Destroy the object if it's not due to be replaced on death.
+    if (!isResetOnDeath)
     {
-        transform.position = originPosition;
-        transform.eulerAngles = originRotation;
-        transform.localScale = originScale;
-        GetComponent<Renderer>().enabled = true;
-        pickedUp = false;
+      Destroy(this.gameObject);
     }
+
+    // Otherwise, simply hide the collectible.
+    GetComponent<Renderer>().enabled = false;
+    pickedUp = true;
+  }
+
+  /// <summary>
+  /// Reset the collectible's position state and visiblility.
+  /// </summary>
+  public virtual void ResetBehaviour()
+  {
+    transform.position = originPosition;
+    transform.eulerAngles = originRotation;
+    transform.localScale = originScale;
+    GetComponent<Renderer>().enabled = true;
+    pickedUp = false;
+  }
 }
