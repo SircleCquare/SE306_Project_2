c5792330b5ce4431272f95f80678ba0ab06f8141
tim hughes <tim hughes>
2016-10-19 13:50:11 +1300

    added gravity to push block

--- Ups and Downs/Assets/_Scripts/Level Scripts/Puzzle Elements/PushableObject.cs
+++ Ups and Downs/Assets/_Scripts/Level Scripts/Puzzle Elements/PushableObject.cs
@@ -8,25 +8,28 @@ using System.Collections;
 
 public class PushableObject : MonoBehaviour  {
 
-	public bool attached;
+    private Rigidbody rb;
+    public bool attached;
 
 	void Start() {
 		attached = false;
-	}
+        rb = GetComponent<Rigidbody>();
+    }
 
 	public void attach(GameObject pushingPlayer) {
 		// TODO: check if player is carrying anything
 		// if player, get the transform and make this parent to transform.
 		this.transform.SetParent (pushingPlayer.transform);
 		attached = true;
-		// TODO: set character's carrying object boolean value
-		// TODO: reduce character speed and jump height if object is heavy
-			
-	}
+        // TODO: set character's carrying object boolean value
+        // TODO: reduce character speed and jump height if object is heavy
+        rb.isKinematic = true;
+    }
 
 	public void detach(){
 		this.transform.SetParent (null);
 		attached = false;
-	}
+        rb.isKinematic = false;
+    }
 	
 }
