c27d92a25196bfe980a7a757e10ecf84487f70c8
Michael Ieti <m.ieti001@gmail.com>
2016-10-21 02:50:37 +1300

    Revert "Minor gameplay tweaks to level 2"
    This reverts commit 523c12a22f2b575df70eeba65958e09f188b0f57.

---
+++ Ups and Downs/Assets/_Scripts/Level Scripts/Killbox.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Killbox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		
+		GameObject collided = other.gameObject;
+
+		if (collided.tag != GameController.PLAYER_TAG) return;
+
+		GameController.Singleton.playerDeath();
+	}
+}
